89021497b075f6cbfab82f9080da122bc1f849cc
alyonaVet <a.v.alenka2015@gmail.com>
2024-08-12 11:53:19 +0600

    added initialize fileDb;

--- api-chat/index.ts
+++ api-chat/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import messagesRouter from "./routers/messages";
+import fileDb from "./fileDb";
 
 const app = express();
 const port = 8000;
@@ -7,8 +8,12 @@ const port = 8000;
 app.use(express.json());
 app.use('/messages', messagesRouter);
 
+const run = async () => {
+    await fileDb.init();
 
+    app.listen(port, () => {
+        console.log(`Server running at http://localhost:${port}`);
+    });
+};
 
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-});
+run().catch(console.error);
