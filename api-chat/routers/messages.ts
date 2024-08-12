ee0b0210f2788c6232d33a46b53de4c425fcd9f5
alyonaVet <a.v.alenka2015@gmail.com>
2024-08-12 12:41:45 +0600

    refactor methods post; added checking if all fields are not empty; refactor "messagesRouter.get"; added get last 30 messages;

--- api-chat/routers/messages.ts
+++ api-chat/routers/messages.ts
@@ -6,17 +6,20 @@ const messagesRouter = express.Router();
 
 messagesRouter.get("/", async (req, res) => {
     const messages = await fileDb.getMessages();
-    return res.send(messages);
+    return res.send(messages.slice(-30));
 });
 
 messagesRouter.post("/", async (req, res) => {
+    if (!req.body.author || !req.body.message) {
+        return res.status(400).send({error: "Author and message must be present in the request"});
+    }
+
     const message: MessageType = {
         author: req.body.author,
         message: req.body.message,
     };
 
     const savedMessage = await fileDb.addMessage(message);
-
 
     return res.send(savedMessage);
 });
