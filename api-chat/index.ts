3a4b5f0b692c1c4cd940b54231caa17399d7f2ac
alyonaVet <a.v.alenka2015@gmail.com>
2024-08-12 15:51:09 +0600

    added corsOptions;

--- api-chat/index.ts
+++ api-chat/index.ts
@@ -1,10 +1,24 @@
 import express from 'express';
+import cors, {CorsOptions} from 'cors';
+
 import messagesRouter from "./routers/messages";
 import fileDb from "./fileDb";
 
 const app = express();
 const port = 8000;
 
+const whitelist = ['http://localhost:5173'];
+const corsOptions: CorsOptions = {
+    origin: (origin, callback) => {
+        if (!origin || whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use('/messages', messagesRouter);
 
