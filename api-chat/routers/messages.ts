86e9ab3a706e0357944e2e50eaaa71a7ffb5d0cb
alyonaVet <a.v.alenka2015@gmail.com>
2024-08-12 12:23:12 +0600

    refactor methods get, post;

--- api-chat/routers/messages.ts
+++ api-chat/routers/messages.ts
@@ -1,13 +1,24 @@
 import express from "express";
+import fileDb from "../fileDb";
+import {MessageType} from "../types";
 
 const messagesRouter = express.Router();
 
-messagesRouter.get("/", (req, res) => {
-    return res.send("Here are your messages: ");
+messagesRouter.get("/", async (req, res) => {
+    const messages = await fileDb.getMessages();
+    return res.send(messages);
 });
 
-messagesRouter.post("/", (req, res) => {
-    return res.send(req.body);
+messagesRouter.post("/", async (req, res) => {
+    const message: MessageType = {
+        author: req.body.author,
+        message: req.body.message,
+    };
+
+    const savedMessage = await fileDb.addMessage(message);
+
+
+    return res.send(savedMessage);
 });
 
 export default messagesRouter;
