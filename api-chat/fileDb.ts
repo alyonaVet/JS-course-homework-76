a53b13e3b2aabc3cacc81c141f6909ff58549325
alyonaVet <a.v.alenka2015@gmail.com>
2024-08-12 12:22:30 +0600

    refactor addMessage; added type MessageType;

--- api-chat/fileDb.ts
+++ api-chat/fileDb.ts
@@ -1,5 +1,6 @@
 import {promises as fs} from 'fs';
-import {IMessage} from "./types";
+import {randomUUID} from "crypto";
+import {IMessage, MessageType} from "./types";
 
 const fileName = './db.json';
 let data: IMessage[] = [];
@@ -16,9 +17,16 @@ const fileDb = {
     async getMessages() {
         return data;
     },
-    async addMessage(message: IMessage) {
-        data.push(message);
+    async addMessage(message: MessageType) {
+        const messageData: IMessage = {
+            id: randomUUID(),
+            ...message,
+            datetime: new Date().toISOString(),
+        };
+
+        data.push(messageData);
         await this.save();
+        return messageData;
     },
     async save() {
         return fs.writeFile(fileName, JSON.stringify(data, null, 2));
